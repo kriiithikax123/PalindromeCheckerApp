0110c395667935bbbc9352d393da09b3b73cb14d
Krithika <krithika.svs24@gmail.com>
2026-02-27 09:55:03 +0530

    Krithika Added UC2 Hardcoded palindrome

---
+++ App/src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,31 @@
+public class UseCase2PalindromeCheckerApp {
+
+    /**
+     * Application entry point for UC2.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Hardcoded input string
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            // Compare characters from both ends
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display input text
+        System.out.println("Input text: " + input);
+
+        // Display result
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
+}
